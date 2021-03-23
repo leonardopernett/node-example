91d78dd86651c8095adbd269ff9383c1c01c50bb
leonardo pernett <lnard.dev@gmail.com>
2021-03-23 14:44:29 -0500

    mejor

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
 
+
+const port = process.env.PORT || 3000
 app.get('/',(req,res)=>{
   res.send('express yeah')
   
 })
 
 
-app.listen(8080);
-console.log('server on port  8080')
+app.listen(port);
+console.log('server on port  3000')
